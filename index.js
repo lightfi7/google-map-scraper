ab075d4091a8fe3443fee63d80a19df5d10c1dda
Fi <158217226+lightfi7@users.noreply.github.com>
2024-04-24 11:09:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const countriesJSON = require("./json/countries.json");
 require("dotenv").config();
 const { io } = require("socket.io-client");
 
-const socket = io("http://localhost:1053");
+const socket = io("http://35.202.239.99:1053");
 
 let googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY;
 
