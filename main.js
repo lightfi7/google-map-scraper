42e0eb91946b3216c2ea492fecefc98f54364daf
Fi <158217226+lightfi7@users.noreply.github.com>
2024-04-26 15:09:43 -0700

    Update main.js

--- main.js
+++ main.js
@@ -252,7 +252,7 @@ const startWork = async () => {
 
 const main = async () => {
   await makeDBConnection();
-  makeSocketConnection("http://35.202.239.99:1053", (socket) => {
+  makeSocketConnection("http://34.170.242.62:1053", (socket) => {
     // makeSocketConnection("http://127.0.0.1:1053", (socket) => {
     config.socket = socket;
 
